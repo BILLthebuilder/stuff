29f6ca7bcd10499f61b11ab4995afd8a3917f9a3
Bill Kariri <billkariri@gmail.com>
2020-02-06 18:34:16 +0300

    create the about component

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>TodoList App</h1>
+          <p>A Todolist App</p>  
+        </React.Fragment>
+    )
+}
